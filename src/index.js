d0777ab6cde76b1f1616c57a84d4be6f421cba3f
Usman Ali Khan <usmanalikhan00@gmail.com>
2020-06-25 02:35:37 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+
+import "./styles.scss";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(ReduxThunk)
+));
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
