71ffc7e0b2c0b391a6cf7a757c4addc76ec77d4b
Usman Ali Khan <usmanalikhan00@gmail.com>
2020-06-25 03:33:06 +0500

    changing API link in api

--- src/api/TrakeApi.js
+++ src/api/TrakeApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:9000'
-  // baseURL: 'https://server125.herokuapp.com'
+  // baseURL: 'http://localhost:9000'
+  baseURL: 'https://server125.herokuapp.com'
 });
